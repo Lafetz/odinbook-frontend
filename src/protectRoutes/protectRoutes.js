c1bdead36692d8004bf8c48e22b1a6bf8ae0d97c
Lafetz <yafetyohaness@gmail.com>
2023-03-22 13:34:09 +0300

    fix route bug in ProtectRoute component

--- src/protectRoutes/protectRoutes.js
+++ src/protectRoutes/protectRoutes.js
@@ -3,6 +3,6 @@ import { Navigate } from "react-router-dom";
 export const ProtectRoutes = ({ children }) => {
   const token = JSON.parse(localStorage.getItem("token"));
   if (!token) {
-    return <Navigate to="/" />;
+    return <Navigate to="/login" />;
   } else return children;
 };
